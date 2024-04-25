5845453b076a090b7b77fcbe96cd171923cfad9d
gabecpf97 <gabrielcpf2997@gmail.com>
2024-04-24 18:03:00 -0700

    fix issue of import in bin/www

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ import indexRouter from "./routes/index.js";
 import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const PORT = 3000;
 
 const app = express();
 
@@ -17,7 +16,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
 
 export default app;
