f366cd2ca2c611207f7f85a2a273c8e8eb536583
gabecpf97 <gabrielcpf2997@gmail.com>
2024-04-24 17:30:52 -0700

    fixing missing type prop in package.json and issue with import

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-import indexRouter from "./routes/index";
+import indexRouter from "./routes/index.js";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const PORT = 3000;
 
 const app = express();
 
@@ -13,6 +17,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
-module.exports = app;
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+
+export default app;
