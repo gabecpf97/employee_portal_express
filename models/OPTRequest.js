a20de7d36e78c66cd2b9e25d86f732155010c74f
gabecpf97 <gabrielcpf2997@gmail.com>
2024-04-25 21:30:49 -0700

    added methods for opt request and updated application create

--- models/OPTRequest.js
+++ models/OPTRequest.js
@@ -1,30 +1,30 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 const refType = Schema.Types.ObjectId;
 
 const OPTRequestSchema = new Schema({
-    userId: { type: refType, ref:"User" },
-    step: { type: Number, required: true }, //indicating which step user at, starts at 1, plus 1 if one file is approved, 4 is max
-    OPTReceipt: {
-        status: { type: String, required: true }, //unuploaded, pending, rejected, approved. Initiate to unuploaded
-        document: {type: String}, //link to file uploaded
-        feedback: {type: String}
-    },
-    OPTEAD: {
-        status: { type: String, required: true }, //unuploaded, pending, rejected, approved. Initiate to unuploaded
-        document: {type: String}, //link to file uploaded
-        feedback: {type: String}
-    },
-    I983: {
-        status: { type: String, required: true }, //unuploaded, pending, rejected, approved. Initiate to unuploaded
-        document: {type: String}, //link to file uploaded
-        feedback: {type: String}
-    },
-    I20: {
-        status: { type: String, required: true }, //unuploaded, pending, rejected, approved. Initiate to unuploaded
-        document: {type: String}, //link to file uploaded
-        feedback: {type: String}
-    },
+  userId: { type: refType, ref: "User" },
+  step: { type: String, required: true }, //indicating which step user at "OPTReceipt" / "OPTEAD" / "I983" / "I20"
+  OPTReceipt: {
+    status: { type: String, required: true }, //unuploaded, pending, rejected, approved. Initiate to unuploaded
+    document: { type: String }, //link to file uploaded
+    feedback: { type: String },
+  },
+  OPTEAD: {
+    status: { type: String, required: true }, //unuploaded, pending, rejected, approved. Initiate to unuploaded
+    document: { type: String }, //link to file uploaded
+    feedback: { type: String },
+  },
+  I983: {
+    status: { type: String, required: true }, //unuploaded, pending, rejected, approved. Initiate to unuploaded
+    document: { type: String }, //link to file uploaded
+    feedback: { type: String },
+  },
+  I20: {
+    status: { type: String, required: true }, //unuploaded, pending, rejected, approved. Initiate to unuploaded
+    document: { type: String }, //link to file uploaded
+    feedback: { type: String },
+  },
 });
 
 const OPTRequest = mongoose.model("OPTRequest", OPTRequestSchema, "OPTRequest");
