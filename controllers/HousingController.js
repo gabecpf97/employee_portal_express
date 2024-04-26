5ed41b176c85f49cf76a39978984b9a9bd1f10dd
Ansel Wang <Anselwang9948@gmail.com>
2024-04-26 17:27:09 -0400

    added HousingController.js this controller is meant to show housing for users and hr, also allow hr to add or delete a housing

--- controllers/HousingController.js
+++ controllers/HousingController.js
@@ -2,9 +2,9 @@ import Housing from '../models/Housing.js';
 import User from '../models/User.js';
 
 const ShowUserHousing = async (req, res) => {
-    const housingrId = req.params.housingId;
+    const housingId = req.params.housingId;
     try {
-        const houseInfo = await Housing.findById(housingrId);
+        const houseInfo = await Housing.findById(housingId);
         return res.status(200).send({ houseInfo})
     } catch (error) {
         return res.status(401).send({
@@ -14,9 +14,19 @@ const ShowUserHousing = async (req, res) => {
 }
 
 const CreateHousing = async (req, res) => {
+    const userId = req.body.userId;
+
     try {
-        const newHousing = await Housing.create(req.body)
-        return res.status(200).send({message: "house created", newHousing});
+        const userInfo = await User.findById(userId);
+        const isHR = userInfo.isHR;
+        if (isHR) {
+            const newHousing = await Housing.create(req.body)
+            return res.status(200).send({message: "house created", newHousing});
+        } else {
+            return res.status(401).send({
+                message: "You are not authorized to create new housing."
+            })
+        }
     } catch (error) {
         return res.status(500).send({
             message: error.message
@@ -24,4 +34,62 @@ const CreateHousing = async (req, res) => {
     }
 }
 
-export { ShowUserHousing, CreateHousing};
+const DeleteHousing = async (req, res) => {
+    const housingId = req.params.housingId;
+    const userId = req.body.userId;
+    // console.log(userId);
+    try {
+        const userInfo = await User.findById(userId);
+        const isHR = userInfo.isHR;
+        if (isHR) {
+            const newHousingList = await Housing.findByIdAndDelete(housingId);
+            if (!newHousingList) {
+                return res.status(404).send({
+                    message: "Housing not found."
+                });
+            }
+
+            return res.status(200).send({
+                message: "Housing has been deleted."
+            })
+        } else {
+            return res.status(401).send({
+                message: "You are not authorized to delete this housing."
+            })
+        }
+
+    } catch (error) {
+        return res.status(500).send({
+            message: error.message
+        })
+    }
+}
+
+const ShowAllHousing = async (req, res) => {
+    const userId = req.body.userId;
+    try {
+        const userInfo = await User.findById(userId);
+        const isHR = userInfo.isHR;
+        if (isHR) {
+            const allHousing = await Housing.find();
+            return res.status(200).send({allHousing});
+        } else {
+            return res.status(401).send({
+                message: "You are not authorized to view all housing."
+            })
+        }
+    } catch (error) {
+        return res.status(500).send({
+            message: error.message
+        });
+    }
+}
+
+
+
+export { 
+    ShowUserHousing, 
+    CreateHousing,
+    DeleteHousing,
+    ShowAllHousing,
+};
