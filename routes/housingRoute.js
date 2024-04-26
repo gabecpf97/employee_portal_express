a202abeebada7f9f569fe078fa8d74baf9f687f2
gabecpf97 <gabrielcpf2997@gmail.com>
2024-04-26 16:29:09 -0700

    fixed issue with import jwtToken

--- routes/housingRoute.js
+++ routes/housingRoute.js
@@ -1,18 +1,16 @@
 import express from "express";
 const router = express.Router();
 import {
-    ShowUserHousing,
-    CreateHousing,
-    DeleteHousing,
-    ShowAllHousing,
+  ShowUserHousing,
+  CreateHousing,
+  DeleteHousing,
+  ShowAllHousing,
 } from "../controllers/HousingController.js";
-import jwtValidation from "../middlewares/AuthMiddleware.js";
-
-
+import { jwtValidation } from "../middlewares/AuthMiddleware.js";
 
 router.post("/", jwtValidation, CreateHousing);
 router.get("/", jwtValidation, ShowAllHousing);
-router.get("/:housingId",jwtValidation, ShowUserHousing);
+router.get("/:housingId", jwtValidation, ShowUserHousing);
 router.delete("/:housingId", jwtValidation, DeleteHousing);
 
 export default router;
