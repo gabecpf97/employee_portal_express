2fb2443bbce1199d1a1679f011c6eba3e9f57a48
gabecpf97 <gabrielcpf2997@gmail.com>
2024-04-26 14:37:20 -0700

    "added mthod to for optrequest controller get and put"

--- controllers/OPTRequestController.js
+++ controllers/OPTRequestController.js
@@ -29,7 +29,7 @@ const optrequest_update_doc = async (req, res, next) => {
 };
 
 // send doc to HR frontend
-const optrequest_get_hr = async (req, res, next) => {
+const optrequest_get_id = async (req, res, next) => {
   try {
     const theOptRequest = await OPTRequest.findById(req.params.id);
     if (!theOptRequest) {
@@ -45,9 +45,83 @@ const optrequest_get_hr = async (req, res, next) => {
   }
 };
 
+// get all request that is in progress
+const optrequest_get_inProgress = async (req, res, next) => {
+  try {
+    const theRequests = await OPTRequest.find({
+      "I20.status": { $ne: "approved" },
+    });
+    if (theRequests.length < 1) {
+      return next({ code: 422, message: "No such OPT requests" });
+    } else {
+      res.status(200).send({ requests: theRequests });
+    }
+  } catch (err) {
+    return next({ code: 500, message: err.message });
+  }
+};
+
+// get all visa request
+const optrequest_get_all = async (req, res, next) => {
+  try {
+    const theRequests = await OPTRequest.find({}).populate({
+      path: "userId",
+      select: "firstName lastName preferredName",
+    });
+    if (theRequests.length < 1) {
+      return next({ code: 422, message: "No OPT request" });
+    } else {
+      const result = theRequests.filter(
+        (request) =>
+          request.userId.firstName === req.query.name ||
+          request.userId.lastName === req.query.name ||
+          request.userId.preferredName === req.query.name
+      );
+      res.status(200).send({ requests: result });
+    }
+  } catch (err) {
+    return next({ err: 500, message: err.message });
+  }
+};
+
+const optrequest_hr_action = async (req, res, next) => {
+  try {
+    const theRequest = await OPTRequest.findById(req.params.id);
+    if (!theRequest) {
+      return next({ code: 422, message: "No such OPT request" });
+    } else {
+      const updateRequest = theRequest;
+      updateRequest[theRequest.step] = {
+        status: req.body.status,
+        document: theRequest[theRequest.step].document,
+        feedback: req.body.feedback,
+      };
+      await OPTRequest.findByIdAndUpdate(req.params.id, updateRequest, {});
+      res.status(200).send({ id: updateRequest._id });
+    }
+  } catch (err) {
+    return next({ err: 500, message: err.message });
+  }
+};
+
+const optrequest_hr_send_noti = async (req, res, next) => {
+  try {
+    const theRequest = await OPTRequest.findById(req.params.id);
+    if (!theRequest) {
+      return next({ code: 422, message: "No such OPT request" });
+    } else {
+    }
+  } catch (err) {
+    return next({ err: 500, message: err.message });
+  }
+};
+
 const optController = {
   optrequest_update_doc,
-  optrequest_get_hr,
+  optrequest_get_id,
+  optrequest_get_inProgress,
+  optrequest_get_all,
+  optrequest_hr_action,
 };
 
 export default optController;
