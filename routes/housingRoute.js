b70b5657095efc0c07276b3bbf9c147e2dedef42
Ansel Wang <Anselwang9948@gmail.com>
2024-04-26 20:25:00 -0400

    added the FacilityReportController.js

--- routes/housingRoute.js
+++ routes/housingRoute.js
@@ -7,10 +7,27 @@ import {
   ShowAllHousing,
 } from "../controllers/HousingController.js";
 import { jwtValidation } from "../middlewares/AuthMiddleware.js";
+import {
+    CreateFacilityReport,
+    ShowUserFacilityReports,
+    GetSingleFacilityReport,
+    PostCommentToReport,
+    GetSingleFacilityComments,
+} from "../controllers/FacilityReportController.js";
 
+
+//HousingController
 router.post("/", jwtValidation, CreateHousing);
 router.get("/", jwtValidation, ShowAllHousing);
 router.get("/:housingId", jwtValidation, ShowUserHousing);
 router.delete("/:housingId", jwtValidation, DeleteHousing);
+
+//FacilitiesController
+router.post("/reports", jwtValidation, CreateFacilityReport);
+router.get("/reports/all", jwtValidation, ShowUserFacilityReports);
+router.get("/reports/:reportId", jwtValidation, GetSingleFacilityReport);
+router.post("/reports/:reportId/comments", jwtValidation, PostCommentToReport )
+router.get("/reports/:reportId/comments", jwtValidation, GetSingleFacilityComments )
+
 
 export default router;
