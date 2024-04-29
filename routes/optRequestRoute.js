9e18b28ddbc466e3faa8ce8c644c7513f5b5e6c1
gabecpf97 <gabrielcpf2997@gmail.com>
2024-04-29 10:46:10 -0700

    added token middleware to optrequest route

--- routes/optRequestRoute.js
+++ routes/optRequestRoute.js
@@ -2,25 +2,41 @@ import express from "express";
 import optController from "../controllers/OPTRequestController.js";
 import optRequestValidator from "../middlewares/OPTRequestMiddleware.js";
 import inputValidaton from "../middlewares/inputMiddleware.js";
+import { jwtValidation, restrictToHR } from "../middlewares/AuthMiddleware.js";
 const optRequestRouter = express.Router();
 
-optRequestRouter.get("/", optController.optrequest_get_all);
-optRequestRouter.get("/in-process", optController.optrequest_get_inProgress);
-optRequestRouter.get("/:id", optController.optrequest_get_id);
+optRequestRouter.get(
+  "/",
+  jwtValidation,
+  restrictToHR,
+  optController.optrequest_get_all
+);
+optRequestRouter.get(
+  "/in-process",
+  jwtValidation,
+  restrictToHR,
+  optController.optrequest_get_inProgress
+);
+optRequestRouter.get("/:id", jwtValidation, optController.optrequest_get_id);
 optRequestRouter.put(
   "/:id",
+  jwtValidation,
   inputValidaton.bodyInputValid,
   optRequestValidator.step_validator,
   optController.optrequest_update_doc
 );
 optRequestRouter.put(
   "/:id/action",
+  jwtValidation,
+  restrictToHR,
   inputValidaton.bodyInputValid,
   optRequestValidator.hr_action_validator,
   optController.optrequest_hr_action
 );
 optRequestRouter.post(
   "/:id/notification",
+  jwtValidation,
+  restrictToHR,
   optController.optrequest_hr_send_noti
 );
 
