b1e3f628c16fe340cc696cec0b904682729dbcd1
xuzy8888 <114893061+xuzy8888@users.noreply.github.com>
2024-04-30 11:48:28 -0500

    Change put user/userid
    this endpoint now only change username, password, email

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,12 +2,13 @@ import express from "express";
 const router = express.Router();
 import { getUserInfo, editUserInfo, getUserStatus } from "../controllers/UserController.js";
 import { jwtValidation, restrictToHR } from "../middlewares/AuthMiddleware.js";
+import { editUserValidation } from "../middlewares/UserMiddleware.js";
 import applicationController from "../controllers/ApplicationController.js";
 
 router.get("/status",jwtValidation, getUserStatus)
 router.get("/search",jwtValidation,restrictToHR,applicationController.application_filter);
 router.get("/:userid", jwtValidation, getUserInfo);
-router.put("/:userid", jwtValidation, editUserInfo);
+router.put("/:userid", jwtValidation, editUserValidation, editUserInfo);
 router.get("/", jwtValidation, restrictToHR, applicationController.application_getAll);
 
 export default router;
